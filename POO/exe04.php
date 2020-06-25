21e40c8546efd6bef525a2ba0073aa8554724799
rvinicius396g <roberto396g@hotmail.com>
2020-06-25 17:42:53 -0300

    Metodo construtor

--- POO/exe04.php
+++ POO/exe04.php
@@ -1,12 +1,38 @@
 <?php
     /*
-        Trabalhando com métodos mágicos / metódo construtor
+        Trabalhando com métodos mágicos / método construtor
     */
     class Endereco {
         private $logradouro;
         private $numero;
         private $cidade;
+
+        //método mágico ou método construtor
+        /*
+            Permite que nós possamos setar valores, ou seja, atribuir valores em a utilização de dos setters
+        */
+        public function __construct($a,$b,$c){
+            $this->logradouro = $a;
+            $this->numero = $b;
+            $this->cidade = $c;
+        }
+
+        public function __destruct(){
+            var_dump("DESTRUIR");
+        }
+
+        public function __tostring(){
+            return $this->logradouro.", ".$this->numero." - ".$this->cidade;
+        }
+
     }
 
+    $meuendereco = new Endereco("Rua X","123","SP");
+
+    //var_dump($meuendereco);
+
+    //unset($meuendereco);
+
+    echo $meuendereco;
 
 ?>
