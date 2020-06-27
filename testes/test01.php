143072da543afd076f0d5addb72066185b3bf3ba
rvinicius396g <roberto396g@hotmail.com>
2020-06-27 14:44:50 -0300

    testes poo atribuicao direta sem geters e setters

---
+++ testes/test01.php
@@ -0,0 +1,15 @@
+<?php
+    class Carros {
+        public $modelo;
+        public $nome;
+        public $ano;
+        public $valor;
+
+    }
+
+    $car0 = new Carros();
+    $car0->modelo = "GM";
+    $car0->nome = "Camaro";
+    $car0->valor = 2500;
+
+?>
